273cd9505de802c85e91cce50510cdbc536103c1
Rogerlinh <manhlinh6393@gmail.com>
2021-03-28 17:43:45 +0700

    1st

---
+++ script.js
@@ -0,0 +1,55 @@
+const APIKEY = "b7fa1a0c70371df20cd5d9b7076adfe8";
+const BASE_URL_WEATHER_ICON = "http://openweathermap.org/img/wn/";
+const IMAGE_SIZE = "@4x.png";
+let form = document.querySelector("#input");
+form.onsubmit = (e) => {
+  e.preventDefault();
+  let city = form.city.value;
+  console.log(city);
+  getData(city);
+};
+
+let getData = async (a) => {
+  try {
+    let response = await axios.get(
+      `https://api.openweathermap.org/data/2.5/weather?q=${a}&appid=${APIKEY}`
+    );
+    console.log(response.data);
+
+    console.log(response.data.weather[0].main.toLowerCase());
+    if (response.data.weather[0].main.toLowerCase() == "mist") {
+      document.getElementById("picWeather").src = `${
+        BASE_URL_WEATHER_ICON + "50d" + IMAGE_SIZE
+      }`;
+      document.getElementById("condition").innerHTML = "Trời âm u";
+    } else if (response.data.weather[0].main.toLowerCase() == "rain") {
+      document.getElementById("picWeather").src = `${
+        BASE_URL_WEATHER_ICON + "09d" + IMAGE_SIZE
+      }`;
+      document.getElementById("condition").innerHTML = "Trời mưa mưa";
+    } else if (response.data.weather[0].main.toLowerCase() == "clear") {
+      document.getElementById("picWeather").src = `${
+        BASE_URL_WEATHER_ICON + "01d" + IMAGE_SIZE
+      }`;
+      document.getElementById("condition").innerHTML = "Trời trong mây thoáng";
+    } else if (response.data.weather[0].main.toLowerCase() == "clouds") {
+      document.getElementById("picWeather").src = `${
+        BASE_URL_WEATHER_ICON + "03d" + IMAGE_SIZE
+      }`;
+      document.getElementById("condition").innerHTML = "Trời có mây";
+    } else {
+      document.getElementById("picWeather").src = `${
+        BASE_URL_WEATHER_ICON + "50d" + IMAGE_SIZE
+      }`;
+      document.getElementById("condition").innerHTML = "Trời ơi!!!";
+    }
+
+    document.getElementById("place").innerHTML = form.city.value;
+    let t = response.data.main.temp - 273;
+    document.getElementById("temp").innerHTML =
+      "Nhiệt độ: " + Math.ceil(t) + "o".sup() + "C";
+  } catch (err) {
+    console.log(err);
+    alert("Bạn nhập sai cmnr");
+  }
+};
